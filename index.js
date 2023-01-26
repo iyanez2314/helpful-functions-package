3f43098489e5cf0daf1e87cecf62c983ffc91eb5
Isaac Yanez <isaac231467@gmail.com>
2023-01-25 21:28:26 -0600

    added some comments to better exlain some of the functions

--- index.js
+++ index.js
@@ -2,14 +2,32 @@
  * This file will hold ALL the functions that will help you save time in your development
  */
 
+/**
+ *
+ * @param {*} object Is an array of objects
+ * @param {*} id id we are looking for
+ * @returns an array with with the object containing the id we are looking for
+ */
 export function findIdIWant(object, id) {
   return object.filter((user) => user.id === id);
 }
 
+/**
+ *
+ * @param {*} min the minimum value we will start the randomization
+ * @param {*} max the maximum value where we will end the randomization
+ * @returns a random number between the minimul and maximum provided values
+ */
 export function generateMinMaxRandomNumbers(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+/**
+ *
+ * @param {*} api api we will fetch
+ * @param {*} apiKey if the api has a bearer token
+ * @returns json data
+ */
 export async function GetRequestFromApiWithKey(api, apiKey) {
   try {
     const response = await fetch(api, {
@@ -28,6 +46,11 @@ export async function GetRequestFromApiWithKey(api, apiKey) {
   }
 }
 
+/**
+ *
+ * @param {*} api fetches from provided api
+ * @returns json data
+ */
 export async function GetRequestFromApi(api) {
   try {
     const response = await fetch(api, {
